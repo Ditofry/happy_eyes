a82fcecff04be27654e645c52afcecfa7d97394f
ditofry <brandon@gocirro.com>
2013-12-14 14:23:09 -0700

    added methods for finding and storing dom info

--- happyEyes.js
+++ happyEyes.js
@@ -3,6 +3,14 @@
  */
 var happyEyes = {
 
+  // List of class/id names we're looking for
+  selectorList: ['main', 'content', 'post', 'article'],
+  domElems: {
+    tags: ['body', 'html'],
+    ids: [],
+    classes: []
+  },
+
   /**
    * Adds class to appropriate element to start CSS3 keyframing process
    *
@@ -14,10 +22,31 @@ var happyEyes = {
   },
 
   // Private methods
+
   findBackgrounds: function(){
+    // Brute force until I learn how to properly wield javascript...
+    var s = this.selectorList;
+    s.forEach(function(name){
+      // Push each match to Dom elem array
+      iMatch = document.getElementById(name);
+      cMatches = document.getElementsByClassName(name);
+
+    });
     console.log('looking for the right divs...');
   },
 
+  // Keepin it dryyyyy
+  pushElem: function(elemm){
+    var t = Object.prototype.toString.call( elemm );
+
+    if ( t = "[object Object]" && elemm !== null ){
+      this.domElems.ids.push(elemm.id);
+    } elseif ( t = "[object Object]" && elemm.length > 0 ) {
+      this.domElems.classes.push(elem.classes);
+    }
+  }
+  //http://stackoverflow.com/questions/6195373/regular-expression-to-get-class-name-with-specific-substring
+  // Keeping this method very standalone for now. Not sure if I'm going to need regexp or not
   hasClass: function(string, klass) {
     var regxp = new RegExp("(?:^| )(" + klass + ")(?: |$)"); 
     var match = ( "" + string ).match( regxp );
@@ -26,6 +55,7 @@ var happyEyes = {
 }
 
 happyEyes.saveEyes();
+happyEyes.findBackgrounds();
 
 
 
